d0fb45554979c9d7a967a6e49995acffe6b05b5d
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-22 19:09:06 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show, :new, :create, :edit, :update]
-  resources :posts, only: [:show, :new, :create, :edit, :update]
+  resources :authors, only: [:show, :new, :create, :edit]
+  patch '/authors/:id', to: 'authors#update'
+  resources :posts, only: [:show, :new, :create, :edit]
+  patch '/posts/:id', to: 'posts#update'
 end
+1
