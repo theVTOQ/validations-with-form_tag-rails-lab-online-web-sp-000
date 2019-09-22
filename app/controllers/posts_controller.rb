2df483ca69e6afae2ae31654f04f6fbd7ae11522
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-22 18:14:33 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,13 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
+
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     @post.update(post_params)
 
     redirect_to post_path(@post)
@@ -20,5 +17,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
   end
 end
