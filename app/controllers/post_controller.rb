6175002c8f315baa0813331bf38a20a29881b0ac
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-22 18:03:09 +0000

    Automatically backed up by Learn

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,10 @@
+class PostController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'post/show'
+
+  get 'post/new'
+
+  get 'post/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
