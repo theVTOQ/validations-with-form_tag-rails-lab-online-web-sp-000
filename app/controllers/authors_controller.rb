d2ab361abd32005bb0fd00a92f39f974663eaf9f
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-22 18:19:05 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,8 +9,9 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
     if @author.is_valid?
+      @author.save
       redirect_to author_path(@author)
     else
       render :new
